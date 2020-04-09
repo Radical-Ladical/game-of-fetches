8e365f9df618ebd46a1c68d8bed5c63ed6976057
Student_M14 <student_m14@PS-DAL-M14.home>
2020-04-09 00:23:08 -0500

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import axios from 'axios';
 import './App.css';
+import components from './components/axios';
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+
+export default class App extends Component {
+  constructo(props) {
+    super(props);
+    this.state= {
+      isLoaded:false,
+      data:[]
+    }
+  }
+  componentDidMount() {
+    let one ="http://anapioficeandfire.com/api/characters/16";
+
+    const requestOne = axios.get(one);
+
+    axios.all([requestOne]).then(axios.spread((...responses) =>{
+      console.log(responses)
+      const responseOne = responses[0].data.born
+
+    })).catch(errors => {
+      console.log ('Error fetching data')
+    })
+    
+  }
+  render() {
+    return(
+      <div>
+        <h2>Where was Margaery Tyrell born?</h2>
+        <h4>{this.state.data[0]}</h4>
+      </div>
+    )
+  }
 }
+
+
+
 
 export default App;
